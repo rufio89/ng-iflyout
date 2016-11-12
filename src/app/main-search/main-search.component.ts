0a47ac924fcf6adee213b988492fd0f5f7074f66
Ryan Krienitz <r.krienitz@gmail.com>
2016-11-12 15:45:11 -0600

    Added date logic. Almost done. ughhhhhhh

--- src/app/main-search/main-search.component.ts
+++ src/app/main-search/main-search.component.ts
@@ -20,11 +20,11 @@ export class MainSearchComponent {
   day: AbstractControl;
   week: AbstractControl;
   daysOfWeek: Day[];
-  numOfWeeks: Week[];
+  numOfMonths: Week[];
   destinations = [];
   departures = [];
   dayOfWeek: number;
-  numWeeks: number;
+  numMonths: number;
   dateList: DateList[];
   private departureService: CompleterData;
   private arrivalService: CompleterData;
@@ -50,7 +50,7 @@ export class MainSearchComponent {
       {id: 6, dayName: 'Saturday'},
       {id: 7, dayName: 'Sunday'}
     ];
-    this.numOfWeeks = [
+    this.numOfMonths = [
       {id:1, nums:1},
       {id:2, nums:2},
       {id:3, nums:3}
@@ -72,61 +72,113 @@ export class MainSearchComponent {
   }
 
   updateDay(dayNum: number){
-    console.log(this.numWeeks);
+    console.log(this.numMonths);
     this.dayOfWeek = dayNum;
-    if(this.numWeeks!== undefined){
+    if(this.numMonths!== undefined){
       this.calculateDates();
     }
 
   }
 
-  updateWeek(weekNum: number){
-    this.numWeeks = weekNum;
+  updateMonth(monthNum: number){
+    this.numMonths = monthNum;
   }
 
-  addDays(day, days: number)
+  addDays(day: Date, days: number)
   {
-    var dat = new Date(day);
+    let dat = new Date(day.getTime());
     dat.setDate(dat.getDate() + days);
     return dat;
   }
 
-  dayDiff(currentDayNum, otherDayNum){
+  addMonths(month, value: number) {
+    let dat = new Date(month);
+    dat.setMonth(dat.getMonth() + value);
+    return dat;
+  };
+
+  dayNumDiff(currentDayNum: number, otherDayNum: number){
     let dayDiff = 0;
-    if(currentDayNum > otherDayNum) {
+    if (currentDayNum > otherDayNum) {
       dayDiff = otherDayNum - currentDayNum;
     }
-    else{
-      dayDiff = currentDayNum - otherDayNum;
+    else {
+      dayDiff = otherDayNum - currentDayNum;
     }
+
     return dayDiff;
   }
 
+  dayDiff(firstDate, secondDate){
+    return secondDate - firstDate;
+  }
+
+  convertDate(d)
+  {
+    var parts = d.split('/');
+    return new Date(parts[1], parts[0]);
+  }
+
   calculateDates(){
+    console.clear();
     let currentDate = new Date();
     let currentDayNum = currentDate.getDay();
-    let dayDiff = this.dayDiff(currentDayNum, this.dayOfWeek);
+    let dayDiff = this.dayNumDiff(currentDayNum, this.dayOfWeek);
     let newCurrentDate = new Date();
 
     newCurrentDate.setDate(currentDate.getDate() + dayDiff);
-    // console.log(currentDayNum);
-    // console.log("Day Num: " + dayDiff);
-
-    //let dd = currentDate.getDate() + dayDiff;
-    //let mm = currentDate.getMonth()+1;
-    //let yyyy = currentDate.getFullYear();
-
-    //let formattedDate = mm + '/' + dd + '/' + yyyy;
-    for(let i=0;i<5;i++){
-      let iterDate = new Date();
-      let mm = newCurrentDate.getMonth()+1;
-      let yyyy = newCurrentDate.getFullYear();
-      let lastDate = new Date(yyyy, mm,0);
-      iterDate = this.addDays(newCurrentDate, 7);
-      newCurrentDate.setDate(iterDate.getDate());
-      if(newCurrentDate > lastDate){
-        newCurrentDate.setMonth(mm+1);
+    // if(currentDate > newCurrentDate){
+    //   newCurrentDate = this.addDays(newCurrentDate, 7);
+    // }
+    let iterDate = newCurrentDate;
+    let mm = iterDate.getMonth()+1;
+    let mm2 = iterDate.getMonth() + this.numMonths;
+    if(mm2 > 12){
+      mm2 = mm2 - 12;
+    }
+    while(mm !=mm2){
+      if(mm>12){
+        mm=0;
       }
+      if(mm==mm2){ break;}
+      console.log(mm);
+      console.log(mm2)
+      //get last day of month
+      let lastDate = new Date(iterDate.getFullYear(), mm,0);
+
+
+      //check if last day in month
+      let currentDayDiff = this.dayDiff(iterDate.getDate(), lastDate.getDate());
+
+      if(currentDayDiff < 7){
+        //Need to increment month by one because it is last day
+        iterDate = new Date(iterDate.getFullYear(),mm,1);
+        //console.log("II: " + iterDate);
+        let newDateDiff = this.dayNumDiff(iterDate.getDay(), this.dayOfWeek);
+        //console.log("AA: " + Number(this.addDays(iterDate, newDateDiff)));
+        //if datediff goes below 0 it brings you back into the previous month
+        let d1 = this.addDays(iterDate, newDateDiff);
+        let d2 = iterDate;
+        // console.log(d1);
+        // console.log(d2);
+        // console.log(d1 < d2);
+        if(d1 < d2){
+          newDateDiff = newDateDiff + 7;
+        }
+        // console.log(newDateDiff);
+        // console.log("iterdate before " + iterDate);
+
+        iterDate.setDate(this.addDays(iterDate, newDateDiff).getDate());
+
+        //console.log("iterdate after" + iterDate);
+        mm=mm+1;
+
+      }
+      else {
+
+        iterDate.setDate(this.addDays(iterDate, 7).getDate());
+      }
+      console.log(iterDate);
 
     }
 
